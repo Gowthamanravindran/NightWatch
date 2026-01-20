54624da658bea3c044f5ed1c7ba6a8f7fa3dff7f
Gowthaman Ravindran <gowthaman.work@gmail.com>
2026-01-20 10:48:40 +0000

    setting up tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  workers: 1, // increase number for parallel tests
   testDir: './src/tests',
   testMatch: ['**/*.web.test.ts'],  
   reporter: [
