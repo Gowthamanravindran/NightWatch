b5986634f2d7d8da9797a3a8c1fb8d98f908e951
Gowthaman Ravindran <gowthaman.work@gmail.com>
2026-01-25 07:48:58 +0000

    fixtures setups

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from '@playwright/test';
+import { loadTestData } from './src/configs/testDataLoader.js';
+
+const testData = loadTestData();
+const workerCount = testData.base_urls?.length || 1;
 
 export default defineConfig({
-  workers: 1, // increase number for parallel tests
+  workers: workerCount,
   testDir: './src/tests',
-  testMatch: ['**/*.web.test.ts'],  
+  testMatch: ['**/*.web.test.ts'],
+  projects: [
+    {
+      name: 'web',
+      use: {
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        trace: 'on-first-retry',
+      },
+      options: {
+        testData,
+      } as any,
+    },
+  ],
   reporter: [
     ['list'],
     [
@@ -16,10 +33,5 @@ export default defineConfig({
       },
     ],
   ],
-  use: {
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    trace: 'on-first-retry'
-  },
   outputDir: 'test-results'
 });
--- src/tests/web/dashboard/dashboard_spec1.web.test.ts
+++ src/tests/web/dashboard/dashboard_spec1.web.test.ts
@@ -1,10 +1,11 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '../../../core/src/fixtures/base.fixture.js';
 import { DashboardPage } from '../../../web/pages/dashboard.js';
 
 test.describe('Booking.com Dashboard', () => {
-    test('Booking.com search returns results', async ({ page }) => {
-       const dashboard = new DashboardPage(page);
-        await page.goto('https://www.booking.com');
+    test('Booking.com search returns results', async ({ page, accountUrl, account }) => {
+        console.log('Using account:', accountUrl);
+        const dashboard = new DashboardPage(page);
+        await page.goto(accountUrl);
         await dashboard.searchFor('Tokyo');
         await expect(page).toHaveURL(/search/);
     });
